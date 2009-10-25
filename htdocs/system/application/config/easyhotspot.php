d9386a3a40c3b5c0be501453ad2b3fe487a2dda5
vcool <rafeequl@gmail.com>
2009-10-25 09:20:20 +0700

    update config easyhotspot.php

--- htdocs/system/application/config/easyhotspot.php
+++ htdocs/system/application/config/easyhotspot.php
@@ -18,17 +18,30 @@ $config['EASYHOTSPOT_THEME']='default';
 
 $config['CHILLISPOT_COFIG_FILE']='/etc/chilli.conf';
 
+#$config['access_controller'] = 'chillispot-hc';
+
 $config['company_name'] = 'EasyHotspot OpenSource';
 $config['company_address_line1'] = 'Legian Street';
 $config['company_address_line2'] = 'Kuta';
 $config['company_address_line3'] = 'Bali - Indonesia';
 $config['company_phone'] = '+62 231 999999';
 
-$config['currency_symbol'] = 'Rp. ';
+$config['currency_symbol'] = '$ ';
+$config['currency_symbol_pdf'] = '$ ';
+$conifg['company_tax_code'] = '';
+$config['admin_price_input'] = 'converted';
+$config['thousands_separator'] = ',';
+$config['decimal_separator'] = '.';
+$config['decimal_places'] = '2';
 
 $config['CHILLISPOT_CONFIG_FILE'] = '/etc/chilli.conf';
 
 $config['postpaid_acct_interim_interval'] = '120';
 $config['voucher_acct_interim_interval'] = '120';
+
+//Freeradius
+$config['radiusserver'] = '127.0.0.1:3799';
+$config['radiuscommand'] = 'radclient -x';
+$config['radiussecret'] = 'easyhotspot';ss
 
 ?>
